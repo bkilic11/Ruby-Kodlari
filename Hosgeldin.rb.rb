b3a6ea5763700107ced8e68d5bc2c5bd8305eaec
yazilim <bariisklc99@gmail.com>
2023-02-21 14:27:18 +0300

    Hoşgeldin Mesajı

---
+++ Hosgeldin.rb.rb
@@ -0,0 +1,7 @@
+print "lütfen kullanıcı adı girin:"
+isim = gets.chop
+if isim == "baris"
+    puts "#{isim} programa hoşgeldin"
+else
+    puts"geçersiz kullanıcı adı girdiniz program sonlandırıldı"
+end
