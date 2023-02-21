9ee645bf86299b4d6b809181df739fc7b2140707
yazilim <bariisklc99@gmail.com>
2023-02-21 14:28:54 +0300

    Kullanıcı Girişi

---
+++ kullaniciGiris.rb
@@ -0,0 +1,9 @@
+print "lütfen kullanıcı adı girin:"
+isim = gets.chop
+if isim == "baris"
+    puts "#{isim} programa hoşgeldin"
+else
+    puts"geçersiz kullanıcı adı girdiniz program sonlandırıldı"
+end
+
+
