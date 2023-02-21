632020975b693a2e06aae31f7117137e10f76011
yazilim <bariisklc99@gmail.com>
2023-02-21 14:28:21 +0300

    Kaldı Geçti Hesaplama

---
+++ KaldiGeçti.rb
@@ -0,0 +1,37 @@
+puts "Notunuzu yazınız:"
+grande = gets.to_i
+if grande == 0
+    puts 'Lütfen bir tam sayı giriniz:'
+grande = gets.to_i
+if grande == 0
+    puts "Sayı girmediniz tekrar deneyiniz!!!"
+    return
+end
+end
+if grande < 0 || grande >100
+    puts '0 ile 100 arasında bir sayı girin:'
+    grande = gets.to_i
+    exit
+else
+    letter_grande =if 90 < grande
+        puts 'AA'
+  elsif grande<=85
+      puts 'BA'
+  elsif grande<=80
+      puts 'BB'
+  elsif grande<=75
+      puts 'CB'
+  elsif grande<=70
+      puts 'CC'
+  elsif grande <= 65
+      puts 'DC'
+  elsif grande <=60
+      puts 'DD'
+  elsif grande <=50
+      puts 'FD'
+  else 
+      puts 'FF'
+end
+
+ puts "Your grade #{letter_grade}"
+end
