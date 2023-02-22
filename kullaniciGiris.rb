3a89bf75e2333dbb53c593ec336d12381cfdc670
Barış Kılıç <113848982+bkilic11@users.noreply.github.com>
2023-02-22 10:53:02 +0300

    Delete kullaniciGiris.rb

--- kullaniciGiris.rb
+++
@@ -1,9 +0,0 @@
-print "lütfen kullanıcı adı girin:"
-isim = gets.chop
-if isim == "baris"
-    puts "#{isim} programa hoşgeldin"
-else
-    puts"geçersiz kullanıcı adı girdiniz program sonlandırıldı"
-end
-
-
